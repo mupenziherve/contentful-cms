0502bd421e5046e4937bb20bed27143771111f64
Mupenzi Herve <mupenziherve04@gmail.com>
2025-10-16 16:29:21 +0200

    update

--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -1,1 +1,7 @@
 import { createClient } from "contentful";
+
+const client = createClient({
+  space: '99ct4foypxej',
+  environment: 'master',
+  accessToken: 'RUvjkNoMgwhW-6soyNnzxGk6Fwed1QElRUQj25SV4q0',
+})
