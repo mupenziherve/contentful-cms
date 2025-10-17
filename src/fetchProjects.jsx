ba510ea531203461957401cc007f41a4f265d84a
Mupenzi Herve <mupenziherve04@gmail.com>
2025-10-17 23:10:42 +0200

    update

--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -5,5 +5,3 @@ const client = createClient({
   environment: 'master',
   accessToken: 'RUvjkNoMgwhW-6soyNnzxGk6Fwed1QElRUQj25SV4q0',
 })
-Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi soluta vitae laudantium possimus sapiente veniam itaque harum, corrupti nesciunt, esse unde fuga, adipisci dignissimos repudiandae reprehenderit aspernatur. Corporis, ad. Hic?
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio veniam, consequuntur, saepe consequatur aliquam nesciunt cupiditate, neque quis aspernatur laborum ducimus unde? Libero distinctio deserunt numquam, quaerat alias sequi cupiditate.
