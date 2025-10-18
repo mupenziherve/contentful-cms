f5b1ce6fcd10282e4dd33ef37139058439aed2b6
Mupenzi Herve <mupenziherve04@gmail.com>
2025-10-18 13:27:34 +0200

    update

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,25 @@
-import './fetchProjects';
+import { useFetchProjects } from './fetchProjects'
 
 const Projects = () => {
+  const { loading, projects } = useFetchProjects()
+
+  if (loading) {
+    return (
+      <section className="projects">
+        <h2>Loading...</h2>
+      </section>
+    )
+  }
+  if (loading) {
+    return (
+      <section className="projects">
+        <div className="title">
+          <h2>projects</h2>
+        </div>
+      </section>
+    )
+  }
+
   return <h2>Projects</h2>
 }
-export default Projects;
+export default Projects
--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -8,12 +8,39 @@ const client = createClient({
   accessToken: 'RUvjkNoMgwhW-6soyNnzxGk6Fwed1QElRUQj25SV4q0',
 });
 
-const useFetchProjects = () =>{
+export const useFetchProjects = () =>{
   const [loading,setLoading] = useState(true)
   const [projects, setProjects] = useState([])
 
+  const getData = async () =>{
+    try {
+      const response = await client.getEntries({ content_type: 'projects' });
+      const  projects =response.items.map((item) =>{
+        const {title, url, image} = item.fields
+        const id = item.sys.id
+        const img = image?.fields?.file?.url
 
-}
+        return {title,url,id,img}
 
-client.getEntries({ content_type: 'projects'})
-.then((response)=> console.log(response));
+      });
+      setProjects(projects);
+
+      setLoading(false)
+      
+    } catch (error) {
+      console.log(error);
+      setLoading(false)
+    }
+  }
+
+  useEffect(()=>{
+    getData()
+
+
+  },[]);
+
+  return {loading, projects};
+
+};
+
+
